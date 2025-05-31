b5bde2982c55d704a9b0ee17ba2c51a07e1e77ef
salman <salmanfarispt18@gmail.com>
2025-05-31 10:08:11 +0530

    time problem changing 1

--- blearn_app/time_restrict_middleware.py
+++ blearn_app/time_restrict_middleware.py
@@ -1,5 +1,5 @@
-from datetime import datetime, time
-from django.http import HttpResponse
+from django.utils.timezone import localtime
+from datetime import time
 from django.shortcuts import render
 
 class TimeRestrictionMiddleware:
@@ -7,15 +7,13 @@ class TimeRestrictionMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        now = datetime.now().time()  # current time only
+        now = localtime().time()  # Gets the current time in the local timezone
 
-        start_time = time(10, 0)  # 10:00 AM
+        start_time = time(10, 0)   # 10:00 AM
         end_time = time(12, 30)    # 12:30 PM
 
-        # Allow access only between 5:00 PM and 6:00 PM
         if start_time <= now <= end_time:
             return self.get_response(request)
 
-        # Block access otherwise
-        return render(request,"site_cant_reached.html",status=403)
-    
+        # Access blocked outside allowed time
+        return render(request, "site_cant_reached.html", status=403)
