2560dbbcb23240ebcd90bd6db99aea606bd8be47
salman <salmanfarispt18@gmail.com>
2025-06-01 13:00:46 +0530

    time changed

--- blearn_app/time_restrict_middleware.py
+++ blearn_app/time_restrict_middleware.py
@@ -10,7 +10,7 @@ class TimeRestrictionMiddleware:
         now = localtime().time()  # Gets the current time in the local timezone
 
         start_time = time(13, 50)   # 10:00 AM
-        end_time = time(14, 5)    # 12:30 PM
+        end_time = time(15, 5)    # 12:30 PM
 
         if start_time <= now <= end_time:
             return self.get_response(request)
