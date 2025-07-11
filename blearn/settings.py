1bb9393813a5e4372d6d9dd66e68d97e081dd788
salman <salmanfarispt18@gmail.com>
2025-07-11 22:29:25 +0530

    added a * on settings.py allowed host

--- blearn/settings.py
+++ blearn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-6=62ygw)gm=i(h&!q!p$dpjoj=)ufjhs@*z@647-jakgope#5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
